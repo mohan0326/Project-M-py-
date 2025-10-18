36569dbdbf7cdb88b43b97d0dc2a9bb77d549b11
mohan0326 <mohanraj2613@gmail.com>
2025-10-18 15:25:37 +0530

    This is first bugfix code

---
+++ bugfix.java
@@ -0,0 +1,9 @@
+public class BugExample {
+    public static void main(String[] args) {
+        String name = null;
+        if (name.equals("John")) {
+            System.out.println("Hello John!");
+        }
+    }
+}
+
